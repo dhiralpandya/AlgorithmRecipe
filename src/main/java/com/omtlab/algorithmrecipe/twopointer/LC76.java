9e3de649275889b3729f30ed378634033ad1ee2b
janudhiral <dhiralpandya@gmail.com>
2019-07-30 09:10:41 -0700

    Added LC 76. Minimum Window Substring

--- src/main/java/com/omtlab/algorithmrecipe/twopointer/LC76.java
+++ src/main/java/com/omtlab/algorithmrecipe/twopointer/LC76.java
@@ -1,4 +1,65 @@
 package com.omtlab.algorithmrecipe.twopointer;
 
+/**
+ * 76. Minimum Window Substring
+ * Given a string S and a string T, find the minimum window in S which will contain all the characters in T in 
+ * complexity O(n).
+ *
+ * Example:
+ *
+ * Input: S = "ADOBECODEBANC", T = "ABC"
+ * Output: "BANC"
+ * Note:
+ *
+ * If there is no such window in S that covers all characters in T, return the empty string "".
+ * If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
+ */
 public class LC76 {
+    
+    
+    public String minWindow(String s, String t){
+        int totalCount = t.length();
+        int mainLenght = s.length();
+        int charCache[] = new int[256];
+        
+        int minLenght = Integer.MAX_VALUE;
+        int minStart = 0;
+        int end = 0;
+        int start = 0;
+        
+        
+        for(Character c: t.toCharArray()){
+            charCache[c]++;
+        }
+        
+        while (end < mainLenght){
+            //charCache[]
+            if(charCache[s.charAt(end)] > 0) {
+                totalCount--;
+            }
+            charCache[s.charAt(end)]--;
+            end++;
+            
+            while(totalCount == 0){
+                if(end-start < minLenght){
+                    minStart = start;
+                    minLenght = end-start;
+                }
+                
+                charCache[s.charAt(start)]++;
+                if(charCache[s.charAt(start)] > 0){
+                    totalCount++;
+                }
+                
+                start++;
+            }
+            
+        }
+        if(minLenght == Integer.MAX_VALUE){
+            return "";
+        }
+        return s.substring(minStart,minStart+minLenght);
+    }
+    
+    
 }
--- src/test/java/com/omtlab/algorithmrecipe/twopointer/LC76Test.java
+++ src/test/java/com/omtlab/algorithmrecipe/twopointer/LC76Test.java
@@ -1,5 +1,41 @@
+package com.omtlab.algorithmrecipe.twopointer;
+
+import com.google.common.collect.Lists;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class LC76Test {
 
+    private String s;
+    private String t;
+    private String output;
+    private LC76 lc76 = new LC76();
+    
+    public LC76Test(String s, String t, String output){
+        this.s = s;
+        this.t = t;
+        this.output = output;
+    }
+    
+    @Parameterized.Parameters
+    public static Collection<Object[]> data(){
+        List<Object[]> data = Lists.newArrayList();
+        
+        data.add(Lists.newArrayList("ADOBECODEBANC","ABC","BANC").toArray());
+        
+        return data;
+    }
+
+    @Test
+    public void minWindow() {
+        Assert.assertEquals(output,lc76.minWindow(s,t));
+    }
 }
