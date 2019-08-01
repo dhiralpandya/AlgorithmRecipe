9bc960ec716628c8ecf2a885f8d8cc8372baaa61
janudhiral <dhiralpandya@gmail.com>
2019-08-01 00:38:07 -0700

    Added LC 173. Binary Search Tree Iterator

--- src/test/java/com/omtlab/algorithmrecipe/tree/inorder/LC173Test.java
+++ src/test/java/com/omtlab/algorithmrecipe/tree/inorder/LC173Test.java
@@ -1,5 +1,29 @@
+package com.omtlab.algorithmrecipe.tree.inorder;
+
+import com.omtlab.algorithmrecipe.common.node.TreeNode;
+import com.omtlab.algorithmrecipe.common.util.TreeUtil;
+import com.omtlab.algorithmrecipe.twopointer.GenerateTree;
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LC173Test {
+    
+    @Test
+    public void test1(){
+        Integer[] input = {7,3,15,null,null,9,20};
+        TreeNode treeNode = new GenerateTree().getRootNode(input);
+        String expectedOutput = "3|7|9|15|20|";
+        
+        LC173 lc173 = new LC173(treeNode);
+        String output = "";
+        while(lc173.hasNext()){
+            output+=lc173.next()+"|";
+        }
+
+        Assert.assertEquals(expectedOutput,output);
+        
+    }
 
 }
