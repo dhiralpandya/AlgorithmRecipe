7ff5c4c2f9c75d6ac89e9b50035a8a554a55ada4
janudhiral <dhiralpandya@gmail.com>
2019-08-07 01:34:11 -0700

    Added LC 43. Multiply Strings

--- src/main/java/com/omtlab/algorithmrecipe/dp/singlearray/LC43.java
+++ src/main/java/com/omtlab/algorithmrecipe/dp/singlearray/LC43.java
@@ -1,4 +1,73 @@
 package com.omtlab.algorithmrecipe.dp.singlearray;
 
+/**
+ * 43. Multiply Strings
+ * Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also 
+ * represented as a string.
+ *
+ * Example 1:
+ *
+ * Input: num1 = "2", num2 = "3"
+ * Output: "6"
+ * Example 2:
+ *
+ * Input: num1 = "123", num2 = "456"
+ * Output: "56088"
+ * Note:
+ *
+ * The length of both num1 and num2 is < 110.
+ * Both num1 and num2 contain only digits 0-9.
+ * Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+ * You must not use any built-in BigInteger library or convert the inputs to integer directly.
+ */
 public class LC43 {
+
+    /**
+     * https://github.com/dhiralpandya/Leetcode/blob/master/src/main/java/com/fishercoder/solutions/_43.java
+     */
+    public String multiplyStrings(String s1, String s2){
+        // s1 * s2
+        
+        int i = s2.length();
+        int j = s1.length();
+        int[] cache = new int[i+j];
+        
+        for(int bottom = 0; bottom<i;bottom++){
+            for(int top = j-1; top>=0; top--){
+                
+                int rightMostForCurrentBottom = bottom+top+1;
+                int positionForCarry = rightMostForCurrentBottom-1;
+                int multiplication = Character.getNumericValue(s2.charAt(bottom))*Character.getNumericValue(s1.charAt(top));
+                
+                cache[rightMostForCurrentBottom] +=multiplication%10;
+                
+                if(cache[rightMostForCurrentBottom] >=10){
+                    int temp = cache[rightMostForCurrentBottom];
+                    cache[rightMostForCurrentBottom]%=10;
+                    cache[positionForCarry]+=temp/10;
+                }
+                cache[positionForCarry]+=multiplication/10;
+                
+                if(cache[positionForCarry] >=10){
+                    int temp = cache[positionForCarry];
+                    cache[positionForCarry]%=10;
+                    cache[positionForCarry-1]+=temp/10;
+                }
+            }
+        }
+        
+        StringBuilder stringBuilder = new StringBuilder();
+        int index = 0;
+        int sumOfAllData = 0;
+        for(int data:cache){
+            if(!(index == 0 && data ==0)){
+                stringBuilder.append(data);
+                sumOfAllData+=data;
+            }
+            index++;
+        }
+        return sumOfAllData ==0?"0": stringBuilder.toString();
+    }
+    
 }
+
--- src/test/java/com/omtlab/algorithmrecipe/dp/singlearray/LC43Test.java
+++ src/test/java/com/omtlab/algorithmrecipe/dp/singlearray/LC43Test.java
@@ -1,5 +1,25 @@
+package com.omtlab.algorithmrecipe.dp.singlearray;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
 import static org.junit.Assert.*;
 
 public class LC43Test {
+    private LC43 lc43 = new LC43();
+    
+    @Test
+    public void multiplyStrings(){
+        Assert.assertEquals(String.valueOf(124*124), lc43.multiplyStrings("124","124"));
+        Assert.assertEquals(String.valueOf(124*1245), lc43.multiplyStrings("124","1245"));
+        Assert.assertEquals(String.valueOf(0*1245), lc43.multiplyStrings("0","1245"));
+        Assert.assertEquals(String.valueOf(new BigInteger("124535673463223").multiply(new BigInteger("1243562341532434"))), lc43.multiplyStrings("124535673463223","1243562341532434"));
+        Assert.assertEquals(String.valueOf(new BigInteger("124356234153243412435623415324343245641243512435623415324341243562341532434324564124351243562341532434124356234153243432456412435")
+                .multiply(new BigInteger("124356234153243412435623415324343245641243512435623415324341243562341532434324564124351243562341532434124356234153243432456412435"))), 
+                lc43.multiplyStrings("124356234153243412435623415324343245641243512435623415324341243562341532434324564124351243562341532434124356234153243432456412435"
+                        ,"124356234153243412435623415324343245641243512435623415324341243562341532434324564124351243562341532434124356234153243432456412435"));
+    }
 
 }
