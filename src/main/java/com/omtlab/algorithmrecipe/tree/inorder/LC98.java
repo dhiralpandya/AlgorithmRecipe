1984976c1a2aba5c305623b581cc2acdf64080b6
janudhiral <dhiralpandya@gmail.com>
2019-08-09 00:00:32 -0700

    Added LC 98. Validate Binary Search Tree

--- src/main/java/com/omtlab/algorithmrecipe/tree/inorder/LC98.java
+++ src/main/java/com/omtlab/algorithmrecipe/tree/inorder/LC98.java
@@ -1,4 +1,141 @@
 package com.omtlab.algorithmrecipe.tree.inorder;
 
-public class Lc98 {
+import com.omtlab.algorithmrecipe.common.node.TreeNode;
+import org.omg.CORBA.TRANSACTION_MODE;
+
+import java.util.Stack;
+
+/**
+ * 98. Validate Binary Search Tree
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ *
+ * Assume a BST is defined as follows:
+ *
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ *
+ *
+ * Example 1:
+ *
+ *     2
+ *    / \
+ *   1   3
+ *
+ * Input: [2,1,3]
+ * Output: true
+ * Example 2:
+ *
+ *     5
+ *    / \
+ *   1   4
+ *      / \
+ *     3   6
+ *
+ * Input: [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is 4.
+ */
+
+
+public class LC98 {
+
+    public static class NodeMinMax{
+        public int min = Integer.MIN_VALUE;
+        public int max = Integer.MAX_VALUE;
+        public boolean isValid;
+        
+        public NodeMinMax(){}
+        public NodeMinMax(int min, int max, boolean isValid){
+            this.min = min;
+            this.max = max;
+            this.isValid = isValid;
+        }
+    }
+
+    
+    
+    public boolean isBST(TreeNode root){
+        NodeMinMax minMax = inOrder(root);
+        return minMax.isValid;
+    }
+    
+    
+    public NodeMinMax inOrder(TreeNode node){
+        if(node == null){
+            return null;
+        }
+        
+        NodeMinMax leftN = inOrder(node.left);
+        if(leftN != null && leftN.max >= node.integerValue){
+            return new NodeMinMax();
+        }
+        
+        NodeMinMax rightN = inOrder(node.right);
+        if(rightN != null && rightN.min <= node.integerValue){
+            return new NodeMinMax();
+        }
+        
+        Integer min  = node.integerValue;
+        Integer max = node.integerValue;
+        if(leftN != null){
+            min = leftN.min;
+        }
+        if(rightN != null){
+            max = rightN.max;
+        }
+        
+        return new NodeMinMax(min,max,true);
+    }
+    
+    
+    /**
+     * Approach 3: Inorder traversal
+     * Algorithm
+     *
+     * Let's use the order of nodes in the inorder traversal Left -> Node -> Right.
+     *
+     * postorder
+     *
+     * Here the nodes are enumerated in the order you visit them, and you could follow 1-2-3-4-5 to compare different
+     * strategies.
+     *
+     * Left -> Node -> Right order of inorder traversal means for BST that each element should be smaller than the 
+     * next one.
+     *
+     * Hence the algorithm with \mathcal{O}(N)O(N) time complexity and \mathcal{O}(N)O(N) space complexity could be 
+     * simple:
+     *
+     * Compute inorder traversal list inorder.
+     *
+     * Check if each element in inorder is smaller than the next one.
+     *
+     * postorder
+     *
+     * Do we need to keep the whole inorder traversal list?
+     *
+     * Actually, no. The last added inorder element is enough to ensure at each step that the tree is BST (or not). 
+     * Hence one could merge both steps into one and reduce the used space.
+     */
+    static class Solution {
+        public boolean isValidBST(TreeNode root) {
+            Stack<TreeNode> stack = new Stack();
+            double inorder = - Double.MAX_VALUE;
+
+            while (!stack.isEmpty() || root != null) {
+                while (root != null) {
+                    stack.push(root);
+                    root = root.left;
+                }
+                root = stack.pop();
+                // If next element in inorder traversal
+                // is smaller than the previous one
+                // that's not BST.
+                if (root.integerValue <= inorder) return false;
+                inorder = root.integerValue;
+                root = root.right;
+            }
+            return true;
+        }
+    }
 }
--- src/test/java/com/omtlab/algorithmrecipe/tree/inorder/LC98Test.java
+++ src/test/java/com/omtlab/algorithmrecipe/tree/inorder/LC98Test.java
@@ -1,5 +1,53 @@
+package com.omtlab.algorithmrecipe.tree.inorder;
+
+import com.google.common.collect.Lists;
+import com.omtlab.algorithmrecipe.common.node.TreeNode;
+import com.omtlab.algorithmrecipe.twopointer.GenerateTree;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class LC98Test {
 
+
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        List<Object[]> data = Lists.newArrayList();
+        
+        Integer a[] = {5,1,4,null,null,3,6};
+        data.add(Lists.newArrayList(new GenerateTree().getRootNode(a),false).toArray());
+
+        Integer b[] = {2,1,3};
+        data.add(Lists.newArrayList(new GenerateTree().getRootNode(b),true).toArray());
+
+        Integer c[] = {1,1};
+        data.add(Lists.newArrayList(new GenerateTree().getRootNode(c),false).toArray());
+
+        Integer d[] = {Integer.MAX_VALUE};
+        data.add(Lists.newArrayList(new GenerateTree().getRootNode(d),true).toArray());
+
+        return data;
+    }
+
+    private  TreeNode root; 
+    private  boolean output;
+    private LC98 lc98 = new LC98();
+    
+    public LC98Test(TreeNode root,boolean output ){
+        this.output = output;
+        this.root = root;
+    }
+    
+    @Test
+    public void isBST() {
+        Assert.assertEquals(output,lc98.isBST(root));
+    }
 }
