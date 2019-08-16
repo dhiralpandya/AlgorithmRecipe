dea1d23247066babe3590a820f1d6948321d1798
janudhiral <dhiralpandya@gmail.com>
2019-08-16 09:21:16 -0700

    Added 139. Word Break

--- src/main/java/com/omtlab/algorithmrecipe/dp/singlearray/startwithzero/LC139.java
+++ src/main/java/com/omtlab/algorithmrecipe/dp/singlearray/startwithzero/LC139.java
@@ -1,5 +1,7 @@
 package com.omtlab.algorithmrecipe.dp.singlearray.startwithzero;
 
+import java.util.List;
+
 /**
  * https://www.youtube.com/watch?v=RPeTFTKwjps&t=829s
  * https://github.com/dhiralpandya/Leetcode/blob/master/src/main/java/com/fishercoder/solutions/_139.java
@@ -28,6 +30,88 @@ package com.omtlab.algorithmrecipe.dp.singlearray.startwithzero;
  * Output: false
  */
 public class LC139 {
+
+
+    /**
+     * Bottom up approach 
+     * let input is 
+     * 
+     * omtlab , [omt, lab]
+     * 
+     * We are solving this using DP so we will consider first step as empty strign then string with lenght 1 and 2 and so on
+     * 
+     * For empty string we will take True
+     * 
+     * Lets take array of n+1, n is length of input string
+     * Length       0|1|2|3|4|5|6
+     *              -------------
+     *              0|o|m|t|l|a|b
+     *              -------------
+     * Current Val  T|F|F|F|F|F|F            
+     * Char Index   -------------
+     *              -|0|1|2|3|4|5
+     *              
+     *  
+     *  For empty string we have True
+     *  
+     *  Lets start with length 1 
+     *  which is 'o' and we will mark it as F
+     *  
+     *  Now lets start with length 2
+     *  At length 2 we will identify both below possibilities
+     *  'om'
+     *  'o' and 'm'
+     *  
+     *  For Length 3
+     *  
+     *  At length 3 we will identify all below possibilities
+     *  
+     *  'om' and 't' : IMP Note > We dont need to consider this if 'om' is false then no need to identify for 't'
+     *  'o' and 'mt'  : IMP Note > We dont need to consider this if 'o' is false then no need to identify for 'mt'
+     *  'omt' = T : Here empty '' string is true so we will check for 'omt'
+     *  
+     *  Now our array will look like this
+     *  
+     *      * Length       0|1|2|3|4|5|6
+     *      *              -------------
+     *      *              0|o|m|t|l|a|b
+     *      *              -------------
+     *      * Current Val  T|F|F|T|F|F|F            
+     *      * Char Index   -------------
+     *      *              -|0|1|2|3|4|5
+     * 
+     * 
+     * 
+     */
+    public boolean wordBreak(String input, List<String> words){
+        
+        //Identify word with MaxLength so our length should not go above that.
+        int maxLenght = -1;
+        
+        for(String word :words){
+            maxLenght = Math.max(word.length(),maxLenght);
+        }
+        
+        boolean cache[] = new boolean[input.length()+1];
+        cache[0] = true;//for zero length
+        for(int length = 1; length <= input.length();length++){
+            for(int rightToLeft=1; rightToLeft <= length && rightToLeft<=maxLenght; rightToLeft++){
+                int currentCacheIndex = length;
+                int previousCacheIndex = length-rightToLeft;
+                if(cache[previousCacheIndex]){
+                    int subStringStart = previousCacheIndex;
+                    int subStringEnd =  length;
+                    String wordToCheck = input.substring(subStringStart,subStringEnd);
+                    if(words.contains(wordToCheck)){
+                        cache[currentCacheIndex]=true;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return cache[input.length()];
+    }
     
-    
+
 }
---
+++ src/test/java/com/omtlab/algorithmrecipe/dp/singlearray/startwithzero/LC139Test.java
@@ -0,0 +1,17 @@
+package com.omtlab.algorithmrecipe.dp.singlearray.startwithzero;
+
+import com.google.common.collect.Lists;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LC139Test {
+
+    @Test
+    public void wordBreak() {
+        Assert.assertTrue(new LC139().wordBreak("omtlab", Lists.newArrayList("omt","lab")));
+        Assert.assertTrue(new LC139().wordBreak("applepenapple", Lists.newArrayList("apple","pen")));
+        Assert.assertFalse(new LC139().wordBreak("catsandog", Lists.newArrayList("cats", "dog", "sand", "and", "cat")));
+    }
+}
