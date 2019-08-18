a308758cb3b0be5380eabdda47e52a93807a84ee
janudhiral <dhiralpandya@gmail.com>
2019-08-17 18:14:24 -0700

    added 670

--- src/main/java/com/omtlab/algorithmrecipe/array/LC670.java
+++ src/main/java/com/omtlab/algorithmrecipe/array/LC670.java
@@ -1,4 +1,35 @@
 package com.omtlab.algorithmrecipe.array;
 
+/**
+ * 
+ * 670. Maximum Swap
+ * 
+ * Given a non-negative integer, you could swap two digits at most once to get the maximum valued number. Return the 
+ * maximum valued number you could get.
+ *
+ * Example 1:
+ * Input: 2736
+ * Output: 7236
+ * Explanation: Swap the number 2 and the number 7.
+ * Example 2:
+ * Input: 9973
+ * Output: 9973
+ * Explanation: No swap.
+ * Note:
+ * The given number is in the range [0, 108]
+ */
 public class LC670 {
+    
+    public int maxValue(int input){
+        char[] allChars = String.valueOf(input).toCharArray();
+        int output = 0;
+        
+        int[] lastIndexOfEachInt = new int[10];
+        for(int i = 0; i < allChars.length; i++){
+            
+        }
+        
+        return output;
+    }
+    
 }
--- src/test/java/com/omtlab/algorithmrecipe/array/LC670Test.java
+++ src/test/java/com/omtlab/algorithmrecipe/array/LC670Test.java
@@ -1,5 +1,15 @@
+package com.omtlab.algorithmrecipe.array;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LC670Test {
 
+    LC670 lc670 = new LC670();
+    
+    @Test
+    public void maxValue() {
+        lc670.maxValue(1); 
+    }
 }
