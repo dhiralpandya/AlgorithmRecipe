a5d7338d51c3cfb4abc7d83dafbe6af8a51a4efa
janudhiral <dhiralpandya@gmail.com>
2019-08-20 22:46:06 -0700

    Added LC 404. Sum of Left Leaves

--- src/main/java/com/omtlab/algorithmrecipe/tree/preorder/LC404.java
+++ src/main/java/com/omtlab/algorithmrecipe/tree/preorder/LC404.java
@@ -1,4 +1,32 @@
 package com.omtlab.algorithmrecipe.tree.preorder;
 
+import com.omtlab.algorithmrecipe.common.node.TreeNode;
+
+/**
+ * 404. Sum of Left Leaves
+ * Find the sum of all left leaves in a given binary tree.
+ *
+ * Example:
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+ */
 public class LC404 {
+    
+    public int total(TreeNode root){
+        if(root == null){
+            return 0;
+        } else if(root.left != null 
+                && root.left.left == null 
+                && root.left.right == null){
+            return root.left.integerValue;
+        } else {
+          return total(root.left) + total(root.right);  
+        }
+    }
 }
--- src/test/java/com/omtlab/algorithmrecipe/tree/preorder/LC404Test.java
+++ src/test/java/com/omtlab/algorithmrecipe/tree/preorder/LC404Test.java
@@ -1,5 +1,19 @@
+package com.omtlab.algorithmrecipe.tree.preorder;
+
+import com.omtlab.algorithmrecipe.common.node.TreeNode;
+import com.omtlab.algorithmrecipe.twopointer.GenerateTree;
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LC404Test {
 
+    @Test
+    public void total() {
+        Integer[] a = {3,9,8,4,0,1,7};
+        TreeNode root = new GenerateTree().getRootNode(a);
+        int total = new LC404().total(root);
+        Assert.assertEquals(5,total);
+    }
 }
