80aea4757743f398ec04e9890a2ec707bc0184ea
janudhiral <dhiralpandya@gmail.com>
2019-08-24 13:02:09 -0700

    Added LC 154. Find Minimum in Rotated Sorted Array II

--- src/test/java/com/omtlab/algorithmrecipe/array/binarysearch/rotated/LC154Test.java
+++ src/test/java/com/omtlab/algorithmrecipe/array/binarysearch/rotated/LC154Test.java
@@ -1,5 +1,16 @@
+package com.omtlab.algorithmrecipe.array.binarysearch.rotated;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LC154Test {
 
+    LC154 lc154 = new LC154();
+    @Test
+    public void findMin() {
+        int[] a = {1,2,2,2,0,1,1};
+        Assert.assertEquals(0,lc154.findMin(a));
+    }
 }
