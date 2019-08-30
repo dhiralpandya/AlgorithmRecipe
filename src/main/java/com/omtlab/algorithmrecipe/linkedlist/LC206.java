602974ecfe3ee1c67a468100c41debeec2e24cc0
janudhiral <dhiralpandya@gmail.com>
2019-08-30 10:01:41 -0700

    Initial Commit for 206

--- src/main/java/com/omtlab/algorithmrecipe/linkedlist/LC206.java
+++ src/main/java/com/omtlab/algorithmrecipe/linkedlist/LC206.java
@@ -1,4 +1,25 @@
 package com.omtlab.algorithmrecipe.linkedlist;
 
-public class LC206 {
+import com.omtlab.algorithmrecipe.common.node.LinkNode;
+
+/**
+ * 206. Reverse Linked List
+ * Reverse a singly linked list.
+ *
+ * Example:
+ *
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * 
+ * Follow up:
+ * -------------------
+ * A linked list can be reversed either iteratively or recursively. Could you implement both?
+ * 
+ */
+public class LC206  {
+    
+    
+    public LinkNode reverse(LinkNode root){
+        
+    }
 }
