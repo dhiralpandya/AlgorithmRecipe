c33f1dcb0d290a1ebbb33d0220e5dcbd97058819
dhiralpandya <dhiralpandya@gmail.com>
2019-08-31 23:46:51 -0700

    Added LC 20. Valid Parentheses

--- src/main/java/com/omtlab/algorithmrecipe/stack/LC20.java
+++ src/main/java/com/omtlab/algorithmrecipe/stack/LC20.java
@@ -1,4 +1,68 @@
 package com.omtlab.algorithmrecipe.stack;
 
+import org.checkerframework.checker.units.qual.C;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * 20. Valid Parentheses
+ * 
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is 
+ * valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ *
+ * Example 1:
+ *
+ * Input: "()"
+ * Output: true
+ * Example 2:
+ *
+ * Input: "()[]{}"
+ * Output: true
+ * Example 3:
+ *
+ * Input: "(]"
+ * Output: false
+ * Example 4:
+ *
+ * Input: "([)]"
+ * Output: false
+ * Example 5:
+ *
+ * Input: "{[]}"
+ * Output: true
+ */
 public class LC20 {
+    
+    private static final Map<Character,Character> CACHE = new HashMap<Character, Character>(){
+        {
+            put('{','}');
+            put('[',']');
+            put('(',')');
+        }  
+    };
+    
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            Character c = s.charAt(i);
+            if(CACHE.containsKey(c)){
+                stack.push(c);
+            } else {
+                if(stack.empty() || CACHE.get(stack.pop()) != c){
+                    return false;
+                }
+            }
+        }
+        
+        return stack.empty();
+    }
 }
--- src/test/java/com/omtlab/algorithmrecipe/stack/LC20Test.java
+++ src/test/java/com/omtlab/algorithmrecipe/stack/LC20Test.java
@@ -1,5 +1,19 @@
+package com.omtlab.algorithmrecipe.stack;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LC20Test {
 
+    LC20 lc20 = new LC20();
+    
+    @Test
+    public void isValid() {
+        Assert.assertTrue(lc20.isValid("()"));
+        Assert.assertTrue(lc20.isValid("()[]{}"));
+        Assert.assertFalse(lc20.isValid("(}"));
+        Assert.assertFalse(lc20.isValid("{(})"));
+    }
 }
