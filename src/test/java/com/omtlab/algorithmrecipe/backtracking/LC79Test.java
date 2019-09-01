f5e774ea75c9afca3b3408a50a65c87a8d8d834f
dhiralpandya <dhiralpandya@gmail.com>
2019-08-31 21:58:02 -0700

    Added LC 79. Word Search

--- src/test/java/com/omtlab/algorithmrecipe/backtracking/LC79Test.java
+++ src/test/java/com/omtlab/algorithmrecipe/backtracking/LC79Test.java
@@ -1,5 +1,22 @@
+package com.omtlab.algorithmrecipe.backtracking;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LC79Test {
 
+    @Test
+    public void exist() {
+        char[][] input = {
+                            {'A','B','C','E'},
+                            {'S','F','C','S'},
+                            {'A','D','E','E'}
+                        };
+
+        Assert.assertTrue(new LC79().exist(input,"ABCCED"));
+        Assert.assertTrue(new LC79().exist(input,"SEE"));
+        Assert.assertFalse(new LC79().exist(input,"ABCB"));
+    }
 }
