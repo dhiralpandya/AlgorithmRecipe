fa0de38ce0f6ee1e76450f70a60609dbb9dd47d3
janudhiral <dhiralpandya@gmail.com>
2019-10-15 00:17:56 -0700

    Added LC 71. Simplify Path

--- src/main/java/com/omtlab/algorithmrecipe/stack/LC71.java
+++ src/main/java/com/omtlab/algorithmrecipe/stack/LC71.java
@@ -3,6 +3,55 @@ package com.omtlab.algorithmrecipe.stack;
 import java.util.*;
 
 /**
+ * 71. Simplify Path
+    Given an absolute path for a file (Unix-style), simplify it.
+    For example,
+        path = "/home/", => "/home"
+        path = "/a/./b/../../c/", => "/c"
+ Corner Cases:
+    Did you consider the case where path = "/../"?
+    In this case, you should return "/".
+    Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+    In this case, you should ignore redundant slashes and return "/home/foo".
+
+ Given an absolute path for a file (Unix-style), simplify it. Or in other words, convert it to the canonical path.
+
+ In a UNIX-style file system, a period . refers to the current directory. Furthermore, a double period .. moves the directory up a level. For more information, see: Absolute path vs relative path in Linux/Unix
+
+ Note that the returned canonical path must always begin with a slash /, and there must be only a single slash / between two directory names. The last directory name (if it exists) must not end with a trailing /. Also, the canonical path must be the shortest string representing the absolute path.
+
+
+
+ Example 1:
+
+ Input: "/home/"
+ Output: "/home"
+ Explanation: Note that there is no trailing slash after the last directory name.
+ Example 2:
+
+ Input: "/../"
+ Output: "/"
+ Explanation: Going one level up from the root directory is a no-op, as the root level is the highest level you can go.
+ Example 3:
+
+ Input: "/home//foo/"
+ Output: "/home/foo"
+ Explanation: In the canonical path, multiple consecutive slashes are replaced by a single one.
+ Example 4:
+
+ Input: "/a/./b/../../c/"
+ Output: "/c"
+ Example 5:
+
+ Input: "/a/../../b/../c//.//"
+ Output: "/c"
+ Example 6:
+
+ Input: "/a//b////c/d//././/.."
+ Output: "/a/b/c"
+ */
+
+/**
  * WORKING FOR ALL INPUT SOLUTION
  */
 class Solution1 {
