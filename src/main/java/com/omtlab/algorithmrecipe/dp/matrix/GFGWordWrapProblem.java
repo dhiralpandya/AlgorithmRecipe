636ab21906ccc19967e570a52737540f13bc6f67
janudhiral <dhiralpandya@gmail.com>
2019-11-10 01:15:02 -0800

    Added LC 68. Text Justification

--- src/main/java/com/omtlab/algorithmrecipe/dp/matrix/GFGWordWrapProblem.java
+++ src/main/java/com/omtlab/algorithmrecipe/dp/matrix/GFGWordWrapProblem.java
@@ -5,71 +5,60 @@ import com.google.common.collect.Lists;
 import java.util.List;
 
 /**
- * 68. Text Justification
+ * Word Wrap Problem | DP-19
+ * Given a sequence of words, and a limit on the number of characters that can be put in one line (line width). Put 
+ * line breaks in the given sequence such that the lines are printed neatly. Assume that the length of each word is 
+ * smaller than the line width.
+ *
+ * The word processors like MS Word do task of placing line breaks. The idea is to have balanced lines. In other 
+ * words, not have few lines with lots of extra spaces and some lines with small amount of extra spaces.
+ *
+ * The extra spaces includes spaces put at the end of every line except the last one.  
+ * The problem is to minimize the following total cost.
+ *  Cost of a line = (Number of extra spaces in the line)^3
+ *  Total Cost = Sum of costs for all lines
+ *
+ * For example, consider the following string and line width M = 15
+ *  "Geeks for Geeks presents word wrap problem" 
+ *
+ * Following is the optimized arrangement of words in 3 lines
+ * Geeks for Geeks
+ * presents word
+ * wrap problem 
+ *
+ * The total extra spaces in line 1, line 2 and line 3 are 0, 2 and 3 respectively. 
+ * So optimal value of total cost is 0 + 2*2 + 3*3 = 13
+ * Please note that the total cost function is not sum of extra spaces, but sum of cubes (or square is also used) of 
+ * extra spaces. The idea behind this cost function is to balance the spaces among lines. For example, consider the 
+ * following two arrangement of same set of words:
+ *
+ *
+ *
+ *
+ *
+ * 1) There are 3 lines. One line has 3 extra spaces and all other lines have 0 extra spaces. Total extra spaces = 3 
+ * + 0 + 0 = 3. Total cost = 3*3*3 + 0*0*0 + 0*0*0 = 27.
+ *
+ * 2) There are 3 lines. Each of the 3 lines has one extra space. Total extra spaces = 1 + 1 + 1 = 3. Total cost = 
+ * 1*1*1 + 1*1*1 + 1*1*1 = 3.
+ *
+ * Total extra spaces are 3 in both scenarios, but second arrangement should be preferred because extra spaces are 
+ * balanced in all three lines. The cost function with cubic sum serves the purpose because the value of total cost 
+ * in second scenario is less.
  * 
- * Given an array of words and a width maxWidth, format the text such that each line has exactly maxWidth characters 
- * and is fully (left and right) justified.
- *
- * You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra 
- * spaces ' ' when necessary so that each line has exactly maxWidth characters.
- *
- * Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line do not 
- * divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
- *
- * For the last line of text, it should be left justified and no extra space is inserted between words.
- *
- * Note:
- *
- * A word is defined as a character sequence consisting of non-space characters only.
- * Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
- * The input array words contains at least one word.
- * Example 1:
- *
- * Input:
- * words = ["This", "is", "an", "example", "of", "text", "justification."]
- * maxWidth = 16
- * Output:
- * [
- *    "This    is    an",
- *    "example  of text",
- *    "justification.  "
- * ]
- * Example 2:
- *
- * Input:
- * words = ["What","must","be","acknowledgment","shall","be"]
- * maxWidth = 16
- * Output:
- * [
- *   "What   must   be",
- *   "acknowledgment  ",
- *   "shall be        "
- * ]
- * Explanation: Note that the last line is "shall be    " instead of "shall     be",
- *              because the last line must be left-justified instead of fully-justified.
- *              Note that the second line is also left-justified becase it contains only one word.
- * Example 3:
- *
- * Input:
- * words = ["Science","is","what","we","understand","well","enough","to","explain",
- *          "to","a","computer.","Art","is","everything","else","we","do"]
- * maxWidth = 20
- * Output:
- * [
- *   "Science  is  what we",
- *   "understand      well",
- *   "enough to explain to",
- *   "a  computer.  Art is",
- *   "everything  else  we",
- *   "do                  "
- * ]
+ * CHECK THIS VIDEO : https://www.youtube.com/watch?v=RORuwHiblPc&t=631s
  */
-public class LC68 {
+public class GFGWordWrapProblem {
 
+    /**
+     *   Find details here : documentation/GFGWordWarp.png
+     */
 
     public List<String> fullJustify(String[] words, int maxWidth) {
 
         int[][] costMatrix = costMatrix(words,maxWidth);
+
+       // MatrixUtil.printMatrix(costMatrix);
         
         int[] breakingCost = new int[words.length];
         int[] breakPoints = new int[words.length];
@@ -81,29 +70,30 @@ public class LC68 {
         int i = words.length-2;
         while (i >= 0){
             
-            breakingCost[i] = costMatrix[i][j]; 
+            breakingCost[i] = costMatrix[i][j]; //We will store i to j value initially here 
             breakPoints[i] = words.length;
             
             while (j > i){
-                if(costMatrix[i][j] == Integer.MAX_VALUE){
+                if(costMatrix[i][j-1] == Integer.MAX_VALUE){
                     j--;
                     continue;
                 }
                 
-                int totalCost = costMatrix[i][j];
-                if(j < words.length-1){
-                    totalCost+=breakingCost[j];
-                }
+                int totalCost = costMatrix[i][j-1]+breakingCost[j];//MOST IMP LINE, Here we are taking i to j-1 from cost martrix and j is from previously calculated breckingCost
                 
                 if(totalCost < breakingCost[i]){
                     breakingCost[i] = totalCost;
                     breakPoints[i] = j == words.length-1 ? words.length:j;
                 }
+                j--;//Please dont forget to reduce
             }
             
             i--;
             j=words.length-1;//Rest J pointer back to last
         }
+
+       // System.out.println(Arrays.toString(breakingCost));
+        //System.out.println(Arrays.toString(breakPoints));
         
         List<String> output = Lists.newArrayList();
         
@@ -111,10 +101,10 @@ public class LC68 {
             StringBuilder sb = new StringBuilder();
             int to = breakPoints[from];
             while(from < to && from < words.length){
-                sb.append(words[from]);
+                sb.append(words[from]+" ");
                 from++;
             }
-                
+                from--;
                 output.add(sb.toString());
         }
         
@@ -129,12 +119,16 @@ public class LC68 {
             for(int column = row; column < words.length; column++){
                 int cost  = maxWidth;
                 int start = row;//Start with current row and move till column
+                int oneSpaceBetweenEachWord = 0;
                 while (start <= column){
                     cost-=words[start].length();
+                    oneSpaceBetweenEachWord++;//For one space between them;
                     start++;
                 }
                 
-                costMatrix[row][column] = cost < 0?Integer.MAX_VALUE:cost;
+                cost-=oneSpaceBetweenEachWord-1;//Not counting last word
+                
+                costMatrix[row][column] = cost < 0?Integer.MAX_VALUE:(int)Math.pow(cost,2);
             }
         }
         
--- src/test/java/com/omtlab/algorithmrecipe/dp/matrix/GFGWordWrapProblemTest.java
+++ src/test/java/com/omtlab/algorithmrecipe/dp/matrix/GFGWordWrapProblemTest.java
@@ -1,5 +1,17 @@
-import static org.junit.Assert.*;
+package com.omtlab.algorithmrecipe.dp.matrix;
+
+import org.junit.Test;
+
+import java.util.List;
 
 public class GFGWordWrapProblemTest {
 
+    @Test
+    public void fullJustify() {
+        String[] input = {"abcdef", "xyz","likes","to", "code"};
+        List<String> output = new GFGWordWrapProblem().fullJustify(input,10);
+        for(String s:output){
+            System.out.println(s);
+        }
+    }
 }
