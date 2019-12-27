30ff06459b9fe52e3546e61cd9100ec643568152
dhpandya <dhpandya@ebay.com>
2019-12-26 17:00:02 -0800

    Added 875. Koko Eating Bananas

--- src/main/java/com/omtlab/algorithmrecipe/binarysearch/LC875.java
+++ src/main/java/com/omtlab/algorithmrecipe/binarysearch/LC875.java
@@ -1,4 +1,88 @@
 package com.omtlab.algorithmrecipe.binarysearch;
 
+/**
+ * 875. Koko Eating Bananas
+ * 
+ * 
+ * Koko loves to eat bananas.  There are N piles of bananas, the i-th pile has piles[i] bananas.  The guards have 
+ * gone and will come back in H hours.
+ *
+ * Koko can decide her bananas-per-hour eating speed of K.  Each hour, she chooses some pile of bananas, and eats K 
+ * bananas from that pile.  If the pile has less than K bananas, she eats all of them instead, and won't eat any more
+ * bananas during this hour.
+ *
+ * Koko likes to eat slowly, but still wants to finish eating all the bananas before the guards come back.
+ *
+ * Return the minimum integer K such that she can eat all the bananas within H hours.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: piles = [3,6,7,11], H = 8
+ * Output: 4
+ * Example 2:
+ *
+ * Input: piles = [30,11,23,4,20], H = 5
+ * Output: 30
+ * Example 3:
+ *
+ * Input: piles = [30,11,23,4,20], H = 6
+ * Output: 23
+ *
+ *
+ * Note:
+ *
+ * 1 <= piles.length <= 10^4
+ * piles.length <= H <= 10^9
+ * 1 <= piles[i] <= 10^9
+ * 
+ */
 public class LC875 {
+
+
+    public int minEatingSpeed(int[] piles, int H) {
+        int output = 0;
+        
+        int start = 1;
+        int end = getSum(piles);
+        
+        while(start <= end){
+            int mid = (start+end)/2;
+            int totalHr = getTotalHrTakesForCurrentMid(mid,piles,H);
+            if(totalHr > H){ //Here K is too small, Eating speed is too low.
+                start = mid+1;
+                output=mid;
+            } else { // K is bigger, Easting speed too fast.
+                end = mid-1;
+            }
+        }
+        
+        
+        return output;
+        
+    }
+    
+    
+    public int getTotalHrTakesForCurrentMid(int mid, int[] piles, int H){
+        int totalHr = 0;
+        
+        for(int pile:piles) {
+            totalHr+= (pile/mid) +1;
+        }
+        
+        return totalHr;
+    }
+    
+    
+    public int getSum(int[] piles){
+        int sum = 0;
+        
+        for(int p : piles){
+            sum+=p;        
+        }
+            
+        return sum;
+    }
+    
 }
--- src/test/java/com/omtlab/algorithmrecipe/binarysearch/LC875Test.java
+++ src/test/java/com/omtlab/algorithmrecipe/binarysearch/LC875Test.java
@@ -1,12 +1,52 @@
 package com.omtlab.algorithmrecipe.binarysearch;
 
+import com.google.common.collect.Lists;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class LC875Test {
 
+    private int[] piles;
+    private int H;
+    private int output;
+    
+    private LC875 lc875 = new LC875();
+    
+    public LC875Test(int[] piles, int H, int output){
+        this.piles = piles;
+        this.H = H;
+        this.output = output;
+    }
+    
+    
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        List<Object[]> list = Lists.newArrayList();
+        int a[] = {3,6,7,11};
+        list.add(Lists.newArrayList(a,8,4).toArray());
+        
+        int b[] = {30,11,23,4,20};
+        list.add(Lists.newArrayList(b,5,30).toArray());
+
+
+        int c[] = {30,11,23,4,20};
+        list.add(Lists.newArrayList(c,6,23).toArray());
+        
+        return list;
+    }
+    
+    
     @Test
     public void minEatingSpeed() {
+        Assert.assertEquals(output,lc875.minEatingSpeed(piles,H));
     }
 }
