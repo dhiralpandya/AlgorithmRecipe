8cd199ccbee58a6f26945356485939a79888127b
dhiralpandya <dhiralpandya@gmail.com>
2020-06-28 18:27:13 -0700

    Added LC 977. Squares of a Sorted Array

---
+++ src/test/java/com/omtlab/algorithmrecipe/array/LC977Test.java
@@ -0,0 +1,27 @@
+package com.omtlab.algorithmrecipe.array;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LC977Test {
+
+    private LC977 lc977 = new LC977();
+
+    @Test
+    void testSortedSquares() {
+        int[] input1 = {-4,-1,0,3,10};
+        int[] output1 = lc977.sortedSquares(input1);
+        int[] expected1 = {0,1,9,16,100};
+        assertEquals(Arrays.toString(expected1), Arrays.toString(output1));
+
+        int[] input2 = {-7,-3,2,3,11};
+        int[] output2 = lc977.sortedSquares(input2);
+        int[] expected2 = {4,9,9,49,121};
+        assertEquals(Arrays.toString(expected2), Arrays.toString(output2));
+    }
+
+
+}
