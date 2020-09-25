304b75034064c8a7b0d89206f5ed717a9eb74496
dhiralpandya <dhiralpandya@gmail.com>
2020-09-24 23:39:02 -0700

    Added LC 398

--- src/main/java/com/omtlab/algorithmrecipe/bit/LC477.java
+++ src/main/java/com/omtlab/algorithmrecipe/bit/LC477.java
@@ -40,7 +40,7 @@ public class LC477 {
     public int totalHammingDistance(int[] nums) {
         int sum = 0;
         
-        for(int i = 1; i <= 32; i++){
+        for(int i = 0; i < 32; i++){
             int digit = 1 << i;
             int ones = 0;
             int zeros = 0;
